d92cea61ffdb1996a074fb9fb7f46cc1a41e3e81
Nate Jones <nate@endot.org>
2017-05-21 13:35:13 -0700

    upload/put works

--- broker.go
+++ broker.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"time"
+
 	"github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/justone/pmb/api"
+	"github.com/pkg/errors"
 )
 
 type BrokerCommand struct {
-	// nothing yet
+	Bucket string `long:"s3-bucket" description:"S3 bucket to use"`
+	Region string `env:"AWS_DEFAULT_REGION" long:"s3-region" description:"S3 region"`
 }
 
 var brokerCommand BrokerCommand
@@ -33,6 +42,20 @@ func init() {
 
 func runBroker(conn *pmb.Connection) error {
 
+	sess := session.Must(session.NewSession())
+
+	var region string
+	if len(brokerCommand.Region) == 0 {
+		var err error
+		region, err = s3manager.GetBucketRegion(aws.BackgroundContext(), sess, brokerCommand.Bucket, endpoints.UsWest2RegionID)
+		if err != nil {
+			return errors.Wrap(err, "unable to determine bucket region")
+		}
+	}
+	s3svc := s3.New(sess, &aws.Config{
+		Region: aws.String(region),
+	})
+
 	for {
 		message := <-conn.In
 		if message.Contents["type"].(string) == "RequestDownloadURL" {
@@ -48,11 +71,26 @@ func runBroker(conn *pmb.Connection) error {
 				}
 			}
 		} else if message.Contents["type"].(string) == "RequestUploadURL" {
+			filename := message.Contents["filename"].(string)
+
+			putObjReq, _ := s3svc.PutObjectRequest(&s3.PutObjectInput{
+				Bucket:        aws.String(brokerCommand.Bucket),
+				Key:           aws.String(filename),
+				ContentLength: aws.Int64(0),
+			})
+			url, headers, err := putObjReq.PresignRequest(15 * time.Minute)
+
+			if err != nil {
+				logrus.Warnf("error presigning: %v", err)
+			}
+
 			logrus.Infof("Generating S3 upload url...")
 			response := map[string]interface{}{
 				"type":       "UploadURLAvailable",
 				"requestor":  message.Contents["id"].(string),
-				"upload_url": "https://s3.aws.com.url/foo.file",
+				"filename":   filename,
+				"upload_url": url,
+				"headers":    headers,
 			}
 			conn.Out <- pmb.Message{Contents: response}
 		}
--- put.go
+++ put.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/justone/pmb/api"
+	"github.com/pkg/errors"
 )
 
 type PutCommand struct {
@@ -35,6 +42,18 @@ func init() {
 
 func runPut(conn *pmb.Connection) error {
 
+	file, err := os.Open(putCommand.Args.FileName)
+	if err != nil {
+		return errors.Wrap(err, "unable to open file")
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		return errors.Wrapf(err, "failed to stat file %s")
+	}
+
+	size := stat.Size()
+
 	request := map[string]interface{}{
 		"type":     "RequestUploadURL",
 		"filename": putCommand.Args.FileName,
@@ -44,12 +63,57 @@ func runPut(conn *pmb.Connection) error {
 	for {
 		message := <-conn.In
 		if message.Contents["type"].(string) == "UploadURLAvailable" {
-			if message.Contents["requestor"].(string) == conn.Id {
-				logrus.Infof("Going to upload %s to %s...", putCommand.Args.FileName, message.Contents["upload_url"].(string))
+			mes := UploadAvailableMessage{}
+			if err := json.Unmarshal([]byte(message.Raw), &mes); err != nil {
+				return errors.Wrap(err, "unable to unmarshal json")
+			}
+
+			// if the upload url message we received was for us and the right filename...
+			if mes.Requestor == conn.Id && mes.Filename == putCommand.Args.FileName {
+				logrus.Infof("Going to upload %s to %s...", putCommand.Args.FileName, mes.Url)
+
+				// build the upload request
+				req, err := http.NewRequest("PUT", mes.Url, nil)
+				if err != nil {
+					return errors.Wrap(err, "unable to build presigned request")
+				}
+
+				// copy over any headers from the signing
+				for key, values := range mes.Header {
+					for _, val := range values {
+						req.Header.Add(key, val)
+					}
+				}
+
+				// set the content length and contnt
+				req.ContentLength = size
+				req.Body = file
+
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					return errors.Wrap(err, "unable to upload file")
+				}
+
+				defer resp.Body.Close()
+
+				if resp.StatusCode != http.StatusOK {
+					body, _ := ioutil.ReadAll(resp.Body)
+					return fmt.Errorf("failed to upload: code: %d, status: %s, body: %s", resp.StatusCode, resp.Status, string(body))
+				}
+
+				// upload is done, and we're done looking for messages
 				break
 			}
 		}
 	}
 
 	return nil
+}
+
+type UploadAvailableMessage struct {
+	Type      string      `json:"type"`
+	Requestor string      `json:"requestor"`
+	Filename  string      `json:"filename"`
+	Url       string      `json:"upload_url"`
+	Header    http.Header `json:"headers"`
 }
